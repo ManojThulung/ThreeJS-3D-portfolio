895322c7f822fa1c591b1c7919f015c2912d1ec7
manoj <manojthulung03@gmail.com>
2023-03-07 21:07:09 +0545

    added 3d computer and completed hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[90px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -23,6 +23,19 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-12 bottom-32 w-full flex justify-center items-center">
+        <a
+          href="#about"
+          className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2"
+        >
+          <motion.dev
+            animate={{ y: [0, 24, 0] }}
+            transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop" }}
+            className="w-3 h-3 rounded-full bg-secondary mb-2"
+          />
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
-  return <div>Loader</div>;
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p className="font-bold text-white-100 mt-4 text-[14px]">
+        {progress.toFixed(1)}%
+      </p>
+    </Html>
+  );
 };
 
-export default Loader;
+export default CanvasLoader;
